afc546f50d26cc16b47022c8bef63b09fba1db13
Tieson18 <tieson360@gmail.com>
2026-05-01 17:52:03 -0500

    fixed list

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -5,7 +5,7 @@ export const MovieController = {
   async getAll(req: Request, res: Response) {
     try {
       const movies = await MovieService.MovieService_list();
-      res.json({ value: movies });
+      res.json(movies);
     } catch (error) {
       res.status(500).json({ error: "Failed to fetch movies" });
     }
